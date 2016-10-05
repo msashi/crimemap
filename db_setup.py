51b481a5f642aad96d08e97a7520d9af6b0eaf55
Sashi Madur <sashi_madur@yahoo.com>
2016-10-05 14:52:03 -0500

    Fix # 2

--- db_setup.py
+++ db_setup.py
@@ -1,7 +1,7 @@
 import pymysql
 import dbconfig
 
-connection = pymysql.connect(host='locahost',
+connection = pymysql.connect(host='localhost',
                              user = dbconfig.db_user,
                              passwd=dbconfig.db_password)
 
